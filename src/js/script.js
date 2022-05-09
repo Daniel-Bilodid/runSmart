e0ee92e06fc0e7816630e0b3ee02c3dae62d0ffe
daniel <danielbeloded@gmail.com>
2022-05-09 15:08:01 +0300

    added php script

--- src/js/script.js
+++ src/js/script.js
@@ -74,4 +74,24 @@ $(document).ready(function(){
     validateForms('#order form');
 
     $('input[name=phone]').mask("+380(99)-999-9999");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        if(!$(this).valid()){
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+    
 });
